a6369695149a382507396e2c52f29f4d4b05ff7a
louisenorris <louisenorris73@yahoo.co.uk>
2019-04-22 19:06:46 +0100

    Completed Implementing Prime lab

--- prime.rb
+++ prime.rb
@@ -1,1 +1,18 @@
+def prime?(x)
+	if x < 2
+		return false
+	end
+	i = 2
+	while i <= x / 2
+		if x % i == 0
+			return false
+		end
+		i += 1
+	end
+	return true
+end
+
+
+	#x % x == 0 && x % 1 == 0
+
 # Add  code here!
